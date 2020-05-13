d7d512a266d15397d75be00ad112bdd73472e17c
khethulwazi kunene <43411903+khethulwazi@users.noreply.github.com>
2020-05-13 19:01:53 +0200

    Update main.cpp

--- Assignments/12-P06/main.cpp
+++ Assignments/12-P06/main.cpp
@@ -1,3 +1,18 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+// Author:           Khethulwazi Kunene
+// Email:            khethokunene@gmail.com
+// Label:            P06
+// Title:            Program 06 - Spanning subtrees uVa problem 11597
+// Course:           3013
+// Semester:         Spring 2020
+//
+// Description:
+//    This program will find the maximum number of spanning trees of 
+//    a complete undirected graph that can be formed in such a way that 
+//    no two of these spanning trees have a common edge.
+//
+/////////////////////////////////////////////////////////////////////////////////
 #include <iostream>
 
 using namespace std;
